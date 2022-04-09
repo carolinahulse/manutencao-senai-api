1431b4644a7a2ce3ed543792dc18d59719739889
Carolina Hulse <carolinahulse321@gmail.com>
2022-04-08 22:27:21 -0300

    Criada a camada service para peça. Realizada refatoração das demais camadas service

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,13 +1,12 @@
 package br.com.senai.manutencaosenaiapi;
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import br.com.senai.manutencaosenaiapi.entity.Tecnico;
+import br.com.senai.manutencaosenaiapi.entity.Peca;
+import br.com.senai.manutencaosenaiapi.service.ClienteService;
 import br.com.senai.manutencaosenaiapi.service.TecnicoService;
 
 @SpringBootApplication
@@ -19,6 +18,10 @@ public class InitApp {
 	
 	@Autowired
 	private TecnicoService service;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
 	@Bean
 	public CommandLineRunner comandLineRunner(ApplicationContext ac) {
 		return args -> {
@@ -28,12 +31,31 @@ public class InitApp {
 				LocalDate dataDeAdmissao = LocalDate.of(2022, 4, 7);
 				novoTecnico.setDataDeAdmissao(dataDeAdmissao);
 				this.service.inserir(novoTecnico);*/
-				Tecnico tecnicoSalvo = new Tecnico();
+				
+				/*Tecnico tecnicoSalvo = new Tecnico();
 				tecnicoSalvo.setId(1);
 				tecnicoSalvo.setNomeCompleto("Joanecleidson");
 				tecnicoSalvo.setDataDeAdmissao(LocalDate.now());
 				this.service.alterar(tecnicoSalvo);
-				System.out.println("Técnico salvo com sucesso");
+				System.out.println("Técnico salvo com sucesso");*/
+				
+				/*this.service.listarPor("Carlos Eduardo");
+				this.service.removerPor(0);*/
+				
+				/*Cliente novoCliente = new Cliente();
+				novoCliente.setNome("João");
+				novoCliente.setSobrenome("da Silva");
+				novoCliente.setCpf("113.586.329-60");
+				novoCliente.setSexo(Sexo.MASCULINO);
+				novoCliente.setEndereco("Rua das Couves");
+				novoCliente.setDataDeNascimento(LocalDate.of(2018, 9, 14));
+				this.clienteService.inserir(novoCliente);
+				System.out.println("Cliente salvo com sucesso");*/
+				
+				Peca novaPeca = new Peca();
+				novaPeca.setDescricao("Teclado");
+				novaPeca.setQtdEmEstoque(10);
+				System.out.println(novaPeca);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
