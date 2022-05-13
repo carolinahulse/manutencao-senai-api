8231ab6bb7e87cbd0076e5a55d538fc250cfa3aa
Carolina Hulse <carolinahulse321@gmail.com>
2022-05-12 22:00:20 -0300

    Inicio do CRUD de peça

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,89 +1,41 @@
 package br.com.senai.manutencaosenaiapi;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import javax.transaction.Transactional;
+import java.awt.EventQueue;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import br.com.senai.manutencaosenaiapi.entity.Cliente;
-import br.com.senai.manutencaosenaiapi.entity.OrdemDeServico;
-import br.com.senai.manutencaosenaiapi.entity.Peca;
-import br.com.senai.manutencaosenaiapi.entity.Tecnico;
-import br.com.senai.manutencaosenaiapi.repository.PecasRepository;
-import br.com.senai.manutencaosenaiapi.repository.TecnicosRepository;
-import br.com.senai.manutencaosenaiapi.service.ClienteService;
-import br.com.senai.manutencaosenaiapi.service.OrdemDeServicoService;
-import br.com.senai.manutencaosenaiapi.service.PecaService;
-import br.com.senai.manutencaosenaiapi.service.TecnicoService;
+import br.com.senai.manutencaosenaiapi.view.TelaCadastroDeTipo;
+
 @SpringBootApplication
 public class InitApp {
+	
+	@Autowired
+	private TelaCadastroDeTipo telaDeCadastroDeTipo;
 
 	public static void main(String[] args) {
-		SpringApplication.run(InitApp.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(InitApp.class);
+		builder.headless(false);
+		builder.run(args);
 	}
 	
-	@Autowired
-	private TecnicoService service;
-	
-	@Autowired
-	private ClienteService clienteService;
-	
-	@Autowired
-	private PecaService pecaService;
-	
-	@Autowired
-	private OrdemDeServicoService ordemService;
-	
-	@Autowired
-	private PecasRepository pecasRepository;
-	
-	@Autowired
-	private TecnicoService tecnicoService;
-	
-	@Autowired
-	private TecnicosRepository tecnicosRepository;
-	
-	@Transactional
 	@Bean
 	public CommandLineRunner comandLineRunner(ApplicationContext ac) {
 		return args -> {
 			try {
-				/*List<Cliente> clientes = clienteService.listarPor("j");
-				Cliente clienteSelecionado = clientes.get(0);
 				
-				List<Tecnico> tecnicos = tecnicoService.listarPor("b");
-				Tecnico tecnicoSelecionado = tecnicos.get(0);
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							telaDeCadastroDeTipo.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
 				
-				List<Peca> pecas = pecaService.listarPor("p");
-				List<Peca> pecasDoReparo = new ArrayList<>();
-				
-				pecasDoReparo.add(pecas.get(0));
-				pecasDoReparo.add(pecas.get(1));
-				
-				OrdemDeServico novaOrdem = new OrdemDeServico();
-				novaOrdem.setCliente(clienteSelecionado);
-				novaOrdem.setTecnico(tecnicoSelecionado);
-				novaOrdem.setDataDeAbertura(LocalDate.of(2022, 05, 06));
-				novaOrdem.setDescricaoDoProblema("Micro não liga");
-				novaOrdem.setPecasDoReparo(pecasDoReparo);
-				
-				ordemService.inserir(novaOrdem);*/
-				
-				/*List<Cliente> clientes = clienteService.listarPor("j");
-				OrdemDeServico ordemSalva = ordemService.buscarPor(6);
-				ordemSalva.setCliente(clientes.get(2));
-				ordemService.alterar(ordemSalva);
-				System.out.println(ordemSalva);*/
-				
-				OrdemDeServico ordemSalva = ordemService.buscarPor(6);
-				ordemSalva.getPecasDoReparo().add(ordemSalva.getPecasDoReparo().get(0));
-				ordemSalva.setDescricaoDoReparo("Poeira");
-				ordemSalva.setDataDeEncerramento(LocalDate.of(2022, 5, 6));
-				ordemService.fechar(ordemSalva);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
