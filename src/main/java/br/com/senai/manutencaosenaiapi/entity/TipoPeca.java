b1875a8e8c6b5d351cbd8b36f68515b962570eea
Carolina Hulse <carolinahulse321@gmail.com>
2022-05-17 21:03:11 -0300

    Atualizações no projeto

--- src/main/java/br/com/senai/manutencaosenaiapi/entity/TipoPeca.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/entity/TipoPeca.java
@@ -11,7 +11,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Entity(name = "TipoPeca")
-@Table(name = "tipos_Pecas")
+@Table(name = "tipos_pecas")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class TipoPeca {
